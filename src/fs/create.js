bf45b7b49fdbce016a7fb23270906b1bb61c1031
Sasha <hlupik69@gmail.com>
2023-06-20 03:26:54 +0300

    feat: add create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import { open } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
+const text = 'I am fresh and young';
+const errorMessage = 'FS operation failed';
+
 const create = async () => {
-    // Write your code here 
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+  try {
+    const file = await open(path.join(__dirname, 'files/fresh.txt'), 'wx');
+    await file.writeFile(text);
+  } catch {
+    throw new Error(errorMessage);
+  }
 };
 
 await create();
